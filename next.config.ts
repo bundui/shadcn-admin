0549944baf92e664ba90d5c27cf6dc4dd70e6cce
toby <hello@tobybelhome.com>
2025-09-16 22:50:14 +0300

    added dotenv

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
+import { config } from "dotenv";
+
+config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  assetPrefix: isProduction ? "https://dashboard.shadcnadmin.com" : undefined,
   images: {
     remotePatterns: [
       {
